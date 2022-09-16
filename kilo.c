fb4df7a9573afadca0cc7860c2e37cee33c5af9e
Oleksandr <o.khytrov@gmail.com>
2022-09-16 21:54:33 +0300

    stages

--- kilo.c
+++ kilo.c
@@ -1,19 +1,31 @@
+/*** includes ***/
 #include <unistd.h>
 #include <stdio.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-//terminal state
+/*** data ***/
+
 struct termios orig_termios;
+
+struct termios orig_termios;
+
+/*** terminal ***/
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode() {
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == 1) {
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == 1) die("tcgetattr");
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | IXON | INPCK | ISTRIP | IXON);
@@ -22,20 +34,21 @@ void enableRawMode() {
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VMIN] = 1;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == 1) die("tcsetattr");
 }
 
+/*** init ***/
 int main() {
     enableRawMode();
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c') \r\n", c, c);
         }
-        if(c == 'q') break;
+        if (c == 'q') break;
     }
     return 0;
 }
